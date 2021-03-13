aa89291cb31f28fd9a0d0b0b264e93a6a43ec018
saitarun135 <saitarun135@gm.com>
2021-03-13 18:16:28 +0530

    Addressbook with credentials

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
 public class AddressBook {
+Scanner sc=new Scanner(System.in);
+	static ContactInfo[] contactList=new ContactInfo[15];
+	int add(int increment)
+	{
+		System.out.println("Enter First Name:");
+		String fn=sc.next();
+		System.out.println("Enter last name:");
+		String ln=sc.next();
+		System.out.println("Enter Address:");
+		String add=sc.next();
+		System.out.println("Enter City:");
+		String city=sc.next();
+		System.out.println("Enter State:");
+		String state=sc.next();
+		System.out.println("Enter postal_code:");
+		String zip=sc.next();
+		System.out.println("Enter Country-code");
+		String country=sc.next();
+		System.out.println("Enter contact number:");
+		String mob=sc.next();
+		System.out.println("Enter email :");
+		String mail=sc.next();
+		contactList[increment++]=new  ContactInfo(fn, ln, add, city, state,zip,country,mob,mail);
+		return increment;
+		
+	}
+	
 	public static void main(String args[]){
-		System.out.println("Wlecome to the book mangement system");
+	AddressBook ab=new AddressBook();		
+	System.out.println("Added to the Addressbook");
 	}
 }
